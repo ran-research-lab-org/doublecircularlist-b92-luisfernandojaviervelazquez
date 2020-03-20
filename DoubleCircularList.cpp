fd65d5f330bee572c9df60ef56c54fab4cb050b3
Luis Fernando Javier Velazquez Sosa <luisfernandojavier.velazquez@upr.edu>
2020-03-20 00:34:59 -0400

    Added somethings to the DoubleCircularList.cpp

--- DoubleCircularList.cpp
+++ DoubleCircularList.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "DoubleCircularList.h"
+bool DoubleCircularList::isEmpty() {
+    if (head == NULL)
+    {
+        return true;
+    }
+    else return false;
+}
+
+void DoubleCircularList::add(const Job &data) {
+
+    DNode* p;
+    DNode *N = new DNode(data, nullptr, nullptr);
+    if (head == NULL) {
+        head = N;
+        N->next = N;
+        N->prev = N;
+    } else {
+        p = head->prev;
+        N->prev = p;
+        p->next = N;
+        N->next = head;
+        head->prev = N;
+    }
+}
+
+DNode* DoubleCircularList::removeCurrent() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
